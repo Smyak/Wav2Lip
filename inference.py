cd394032e9517d3b408cc8282321b143f7503053
andrey <smyaksmyak@gmail.com>
2023-03-15 19:33:48 +0300

    Skip frames without faces

--- inference.py
+++ inference.py
@@ -88,15 +88,16 @@ def face_detect(images):
 	pady1, pady2, padx1, padx2 = args.pads
 	for rect, image in zip(predictions, images):
 		if rect is None:
-			cv2.imwrite('temp/faulty_frame.jpg', image) # check this frame where the face was not detected.
-			raise ValueError('Face not detected! Ensure the video contains a face in all the frames.')
+			#cv2.imwrite('temp/faulty_frame.jpg', image) # check this frame where the face was not detected.
+			#raise ValueError('Face not detected! Ensure the video contains a face in all the frames.')
+			results.append([0, 0, 1, 1])
+		else:
+			y1 = max(0, rect[1] - pady1)
+			y2 = min(image.shape[0], rect[3] + pady2)
+			x1 = max(0, rect[0] - padx1)
+			x2 = min(image.shape[1], rect[2] + padx2)
 
-		y1 = max(0, rect[1] - pady1)
-		y2 = min(image.shape[0], rect[3] + pady2)
-		x1 = max(0, rect[0] - padx1)
-		x2 = min(image.shape[1], rect[2] + padx2)
-		
-		results.append([x1, y1, x2, y2])
+			results.append([x1, y1, x2, y2])
 
 	boxes = np.array(results)
 	if not args.nosmooth: boxes = get_smoothened_boxes(boxes, T=5)
